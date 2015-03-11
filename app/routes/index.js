df2c4df874fb68e2e24ef9771157d2c577527104
Fred Wenzel <fwenzel@mozilla.com>
2015-03-10 17:00:06 -0700

    Automated upgrade code from FireKey 1.0. Time to upstream. Fix #2.

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,13 +5,35 @@ export default Ember.Route.extend({
     var route = this;
 
     this.store.find('key').then(function(keys) {
-    	if (keys.content.length) {
-	    	// Main page since there are keys to show.
-	    	route.transitionTo('keys');
-	    } else {
-	    	// nothing to show.
-	    	route.transitionTo('welcome');
-	    }
+      if (keys.content.length) {
+        // Main page since there are keys to show.
+        route.transitionTo('keys');
+      } else {
+        /**
+         * Firekey upgrade code from version 1.0.
+         * If we find old keys in IndexedDB, let's fetch them and upgrade.
+         */
+        window.localforage.getItem('accounts').then(function(accounts) {
+          if (accounts) {
+            // Create models for each known key.
+            for (var i=0; i<accounts.length; i++) {
+              var newKey = route.store.createRecord('key');
+              newKey.set('name', accounts[i]['name']);
+              newKey.set('key', accounts[i]['key']);
+              newKey.save();
+            }
+
+            // Remove the old data.
+            window.localforage.removeItem('accounts');
+
+            // Now we *do* have something to show.
+            route.transitionTo('keys');
+          } else {
+            // nothing to show.
+            route.transitionTo('welcome');
+          }
+        });
+      }
     });
   }
 });
